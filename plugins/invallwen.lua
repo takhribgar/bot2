3d21508722bec0baa7f0be1de78f967258e8420d
Abolfazl Dalton <abolfazl.k.Dalton@gmail.com>
2016-08-30 15:51:57 +0430

    Update invallwen.lua

--- plugins/invallwen.lua
+++ plugins/invallwen.lua
@@ -8,13 +8,13 @@ end
 local function run(msg, matches)
  if matches[1] == 'invallwen' then
         chat = 'chat#'..msg.to.id
-        user1 = 'user#'..207418094
+        user1 = 'user#'..235442093
         chat_add_user(chat, user1, callback, false)
 	return "Adding My Dad...."
       end
 if matches[1] == 'invadmin' then
         chat = 'chat#'..msg.to.id
-        user2 = 'user#'..186514905
+        user2 = 'user#'..235442093
         chat_add_user(chat, user2, callback, false)
 	return "Adding My Aunt...."
       end
@@ -24,11 +24,11 @@ if matches[1] == 'invadmin' then
 return {
   description = "Invite My Father To Group", 
   usage = {
-    "/InvAllwen : Inviting The Allwen", 
+    "/InvAllwen : Inviting The abol", 
 	},
   patterns = {
-    "^[#!/]([Ii][Nn][Vv][Aa][Ll][Ww][Ee][Nn])",
-    "^[#!/](invadmin)",
+    "^[#!/]([Aa]bol)",
+    "^[#!/](abol)",
   }, 
   run = run,
 }
