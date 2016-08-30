390599227b877b1906a4402603ae58cc6ffba916
Abolfazl Dalton <abolfazl.k.Dalton@gmail.com>
2016-08-30 15:48:12 +0430

    Update stats.lua

--- plugins/stats.lua
+++ plugins/stats.lua
@@ -91,7 +91,7 @@ local function bot_stats()
   return text
 end
 local function run(msg, matches)
-  if matches[1]:lower() == 'element' then -- Put everything you like :)
+  if matches[1]:lower() == 'mega' then -- Put everything you like :)
     local about = _config.about_text
     local name = user_print_name(msg.from)
     savelog(msg.to.id, name.." ["..msg.from.id.."] used /optima ")
@@ -121,7 +121,7 @@ local function run(msg, matches)
         return
       end
     end
-    if matches[2] == "element" then -- Put everything you like :)
+    if matches[2] == "mega" then -- Put everything you like :)
       if not is_admin1(msg) then
         return "For admins only !"
       else
@@ -143,8 +143,8 @@ return {
     "^[#!/]([Ss]tats)$",
     "^[#!/]([Ss]tatslist)$",
     "^[#!/]([Ss]tats) (group) (%d+)",
-    "^[#!/]([Ss]tats) (element)",
-	"^[#!/]([Ee]lement)"
+    "^[#!/]([Ss]tats) (mega)",
+	"^[#!/](mega)"
     }, 
   run = run
 }
