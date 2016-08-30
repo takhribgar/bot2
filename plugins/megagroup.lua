b823e7500beb84cde3577ca9552e94ed6a6f6fb0
Abolfazl Dalton <abolfazl.k.Dalton@gmail.com>
2016-08-30 17:14:55 +0430

    Update megagroup.lua

--- plugins/megagroup.lua
+++ plugins/megagroup.lua
@@ -2628,7 +2628,7 @@ local function run(msg, matches)
 		end
 
 		if matches[1] == 'help' and not is_owner(msg) then
-			text = "Send /superhelp in private @ElementTG Or wait for group owner"
+			text = "help list @myth_Typer"
 			reply_msg(msg.id, text, ok_cb, false)
 		elseif matches[1] == 'help' and is_owner(msg) then
 			local name_log = user_print_name(msg.from)
