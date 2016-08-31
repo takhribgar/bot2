f4ef4bc338737e475f39d7af92100b1bf96f339d
Abolfazl Dalton <abolfazl.k.Dalton@gmail.com>
2016-08-31 12:07:05 +0430

    Update addplug.lua

--- plugins/addplug.lua
+++ plugins/addplug.lua
@@ -1,24 +1,37 @@
-local function run(msg, matches)
-  if is_sudo(msg) then
-  local text = matches[1]
-  local b = 1
-  while b ~= 0 do
-    text = text:trim()
-    text,b = text:gsub('^!+','')
+local function savefile(extra, success, result)
+  local msg = extra.msg
+  local name = extra.name
+  local adress = extra.adress
+  local receiver = get_receiver(msg)
+  if success then
+    local file = './'..adress..'/'..name..''
+    print('File saving to:', result)
+    os.rename(result, file)
+    print('File moved to:', file)
+  else
+    print('Error downloading: '..msg.id)
+    send_large_msg(receiver, 'Failed, please try again!', ok_cb, false)
   end
-  local name = matches[2]
-  local file = io.open("./plugins/"..name..".lua", "w")
-  file:write(text)
-  file:flush()
-  file:close()
-  return "Reval Shod ;-)" 
 end
-end 
+local function run(msg,matches)
+    local receiver = get_receiver(msg)
+    local group = msg.to.id
+    if msg.reply_id then
+    local adress = matches[2]
+   local name = matches[3]
+      if matches[1] == "file" and matches[2] and matches[3] and is_sudo(msg) then
+load_document(msg.reply_id, savefile, {msg=msg,name=name,adress=adress})
+        return 'File '..name..' has been saved in: \n./'..adress
+      end
+      
+         if not is_sudo(msg) then
+           return "You Are Not mmd :/"
+         end
+end
+end
 return {
-  description = "a Usefull plugin for sudo !",  
-  usage = "A plugins to add Another plugins to the server",
   patterns = {
-    "^#addplug +(.+) (.*)$"
+ "^[!/#]([Ff]ile) (.*) (.*)$",
   },
-  run = run
+  run = run,
 }
