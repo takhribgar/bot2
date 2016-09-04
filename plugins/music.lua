92cf167cddb60938d315f4c7c5f10f05012896d8
Abolfazl Dalton <abolfazl.k.Dalton@gmail.com>
2016-09-05 00:26:01 +0430

    Update music.lua

--- plugins/music.lua
+++ plugins/music.lua
@@ -3,7 +3,7 @@ local function musiclink(msg, musicid)
  if not value then
   return
  else
-  value = value..'\n\n@ElementTeam'
+  value = value..'\n\n@Myth_Typer'
   return value
  end
 end
@@ -36,7 +36,7 @@ function run(msg, matches)
   if not value then
    return 'آهنگ مورد نظر پیدا نشد.'
   else
-   value = value..'\n\n@ElementTeam'
+   value = value..'\n\n@myth_Typer'
    return value
   end
   return
