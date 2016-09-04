b450007e99bd90a23ca2666c1f36f6d7b9ef30fb
Abolfazl Dalton <abolfazl.k.Dalton@gmail.com>
2016-09-04 11:50:15 +0430

    Update tag.lua

--- plugins/tag.lua
+++ plugins/tag.lua
@@ -1,76 +1,43 @@
-local function run(msg, matches)
-    if is_owner(msg) then
-        return
-    end
-    local data = load_data(_config.moderation.data)
-    if data[tostring(msg.to.id)] then
-        if data[tostring(msg.to.id)]['settings'] then
-            if data[tostring(msg.to.id)]['settings']['fosh'] then
-                lock_fosh = data[tostring(msg.to.id)]['settings']['fosh']
-            end
-        end
-    end
-    local chat = get_receiver(msg)
-    local user = "user#id"..msg.from.id
-    if lock_fosh == "yes" then
-        send_large_msg(chat, '')
-        chat_del_user(chat, user, ok_cb, true)
-    end
-end
+do
+
+local function pre_process(msg)
     
-return {patterns = {
-    "کیر",
-    "[Kk]ir",
-    "کص",
-    "کس",
-    "جنده",
-    "لاشی",
-    "کونی",
-    "کس(.*)",
-    "کون(.*)",
-    "کیر(.*)",
-    "ممه(.*)",
-    "سکس(.*)",
-    "سیکتیر(.*)",
-    "قهبه(.*)",
-    "بسیک(.*)",
-    "بیناموس(.*)",
-    "اوبی(.*)",
-    "کونی(.*)",
-    "بیشرف(.*)",
-    "کس ننه(.*)",
-    "ساک(.*)",
-    "کیری(.*)",
-    "خار کوسه(.*)",
-    "ننه(.*)",
-    "خواهرتو(.*)",
-    "سکسی(.*)",
-    "کسکش(.*)",
-    "سیک تیر(.*)",
-    "گاییدم(.*)",
-    "میگام(.*)",
-    "میگامت(.*)",
-    "بسیک(.*)",
-    "خواهرت(.*)",
-    "خارتو(.*)",
-    "کونت(.*)",
-    "کوست(.*)",
-    "شورت(.*)",
-    "سگ(.*)",
-    "کیری(.*)",
-    "دزد(.*)",
-    "ننت(.*)",
-    "ابمو(.*)",
-    "سگ",
-    "مادر سگ",
-    "[Kk]os",
-    "[Jj]ende",
-    "[Ll]ashi",   
-    "[Kk]ooni",
-    "[Hh]aroom",
-    "[Ff]uck",
-    "[Ff]cker",
-    "suck"
+    --Checking mute
+    local hash = 'mate:'..msg.to.id
+    if redis:get(hash) and msg.reply_id and not is_sudo(msg) and not is_owner(msg) and not is_momod(msg) and not is_admin1(msg) then
+            delete_msg(msg.id, ok_cb, true)
+            return ""
+        end
+    
+        return msg
+    end
+
+  
+
+
+local function run(msg, matches)
+    chat_id =  msg.to.id
+    
+    if is_momod(msg) and matches[1] == 'lock' then
+      
+            
+                    local hash = 'mate:'..msg.to.id
+                    redis:set(hash, true)
+                    return ""
+  elseif is_momod(msg) and matches[1] == 'unlock' then
+                    local hash = 'mate:'..msg.to.id
+                    redis:del(hash)
+                    return ""
+end
+
+end
+
+return {
+    patterns = {
+        '^[/!#](lock) reply$',
+        '^[/!#](unlock) reply$'
     },
-    run = run
-    }
+    run = run,
+    pre_process = pre_process
+}
+end
