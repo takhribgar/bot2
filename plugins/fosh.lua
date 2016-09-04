b36efdb1b408a1bb4659603c8d3c23809771e28b
Abolfazl Dalton <abolfazl.k.Dalton@gmail.com>
2016-09-04 22:26:47 +0430

    Update fosh.lua

--- plugins/fosh.lua
+++ plugins/fosh.lua
@@ -1,28 +1,53 @@
+plugins/Lock_English.lua
+
 local function run(msg, matches)
-    if is_owner(msg) then
+    if is_momod(msg) then
         return
     end
     local data = load_data(_config.moderation.data)
     if data[tostring(msg.to.id)] then
         if data[tostring(msg.to.id)]['settings'] then
-            if data[tostring(msg.to.id)]['settings']['tag'] then
-                lock_tag = data[tostring(msg.to.id)]['settings']['tag']
+            if data[tostring(msg.to.id)]['settings']['english'] then
+                lock_english = data[tostring(msg.to.id)]['settings']['english']
             end
         end
     end
     local chat = get_receiver(msg)
     local user = "user#id"..msg.from.id
-    if lock_tag == "yes" then
-        send_large_msg(chat, 'tag is not allowed here'
-        chat_del_user(chat, user, ok_cb, true)
+    if lock_english == "yes" then
+       delete_msg(msg.id, ok_cb, true)
     end
 end
  
 return {
   patterns = {
-    "#(.*)",
-    "@(.*)"
+"[Aa](.*)",
+"[Bb](.*)",
+"[Cc](.*)",
+"[Dd](.*)",
+"[Ee](.*)",
+"[Ff](.*)",
+"[Gg](.*)",
+"[Hh](.*)",
+"[Ii](.*)",
+"[Jj](.*)",
+"[Kk](.*)",
+"[Ll](.*)",
+"[Mm](.*)",
+"[Nn](.*)",
+"[Oo](.*)",
+"[Pp](.*)",
+"[Qq](.*)",
+"[Rr](.*)",
+"[Ss](.*)",
+"[Tt](.*)",
+"[Uu](.*)",
+"[Vv](.*)",
+"[Ww](.*)",
+"[Xx](.*)",
+"[Yy](.*)",
+"[Zz](.*)",
   },
   run = run
 }
-
+ Desktop version
