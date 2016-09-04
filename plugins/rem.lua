8ea5a69bf3a81c2ca88e8a14006e92b9bd2615b6
Abolfazl Dalton <abolfazl.k.Dalton@gmail.com>
2016-09-05 01:02:33 +0430

    Update rem.lua

--- plugins/rem.lua
+++ plugins/rem.lua
@@ -3,29 +3,29 @@ local function history(extra, suc, result)
     delete_msg(result[i].id, ok_cb, false)
   end
   if tonumber(extra.con) == #result then
-    send_msg(extra.chatid, '"'..#result..'" 倬蹖丕賲 丕禺蹖乇 爻賵倬乇 诏乇賵賴 丨匕賮 卮丿', ok_cb, false)
+    send_msg(extra.chatid, '"'..#result..'" پیام اخیر سوپر گروه حذف شد', ok_cb, false)
   else
-    send_msg(extra.chatid, '鬲毓丿丕丿 倬蹖丕賲 賲賵乇丿 賳馗乇 卮賲丕 倬丕讴 卮丿', ok_cb, false)
+    send_msg(extra.chatid, 'تعداد پیام مورد نظر شما پاک شد', ok_cb, false)
   end
 end
 local function run(msg, matches)
-  if matches[1] == 'dlpm' and is_owner(msg) then
+  if matches[1] == 'clean' and is_owner(msg) then
     if msg.to.type == 'channel' then
-      if tonumber(matches[2]) > 1000000 or tonumber(matches[2]) < 1 then
-        return "鬲毓丿丕丿 亘蹖卮鬲乇 丕夭 1 賲噩丕夭 丕爻鬲"
+      if tonumber(matches[2]) > 10000 or tonumber(matches[2]) < 1 then
+        return "تعداد بیشتر از 1 مجاز است"
       end
       get_history(msg.to.peer_id, matches[2] + 1 , history , {chatid = msg.to.peer_id, con = matches[2]})
     else
-      return "賮賯胤 丿乇 爻賵倬乇诏乇賵賴 賲賲讴賳 丕爻鬲"
+      return "فقط در سوپرگروه ممکن است"
     end
   else
-    return "卮賲丕 丿爻鬲乇爻蹖 賳丿丕乇蹖丿"
+    return "شما دسترسی ندارید"
   end
 end
 
 return {
     patterns = {
-        '^[!/#](dlpm) (%d*)$'
+        '^[!/#]([Rr]mg) (%d*)$'
     },
     run = run
 }
