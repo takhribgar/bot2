02f62ed06b8977fdba550ab5b5e206957fd43a10
Abolfazl Dalton <abolfazl.k.Dalton@gmail.com>
2016-09-05 01:08:52 +0430

    Update rem.lua

--- plugins/rem.lua
+++ plugins/rem.lua
@@ -1,31 +1,42 @@
-local function history(extra, suc, result)
-  for i=1, #result do
-    delete_msg(result[i].id, ok_cb, false)
-  end
-  if tonumber(extra.con) == #result then
-    send_msg(extra.chatid, '"'..#result..'" پیام اخیر سوپر گروه حذف شد', ok_cb, false)
-  else
-    send_msg(extra.chatid, 'تعداد پیام مورد نظر شما پاک شد', ok_cb, false)
-  end
-end
-local function run(msg, matches)
-  if matches[1] == 'clean' and is_owner(msg) then
-    if msg.to.type == 'channel' then
-      if tonumber(matches[2]) > 10000 or tonumber(matches[2]) < 1 then
-        return "تعداد بیشتر از 1 مجاز است"
-      end
-      get_history(msg.to.peer_id, matches[2] + 1 , history , {chatid = msg.to.peer_id, con = matches[2]})
-    else
-      return "فقط در سوپرگروه ممکن است"
-    end
-  else
-    return "شما دسترسی ندارید"
-  end
-end
-
-return {
-    patterns = {
-        '^[!/#]([Rr]mg) (%d*)$'
-    },
-    run = run
-}
+  GNU nano 2.2.6 File: .../ashi/a/plugins/rem.lua           
+                                                            
+local function history(extra, suc, result)                  
+for i=1, #result do                                         
+delete_msg(result[i].id, ok_cb, false)                      
+end                                                         
+if tonumber(extra.con) == #result then                      
+send_msg(extra.chatid, ''..#result..'Massages has been remo$
+else                                                        
+send_msg(extra.chatid, 'All selected massages has been remo$
+end                                                         
+end                                                         
+local function run(msg, matches)                            
+if matches[1] == 'rem' and is_owner(msg) then               
+            if msg.to.type == 'channel' then                
+            if tonumber(matches[2]) > 999 or tonumber(match$
+            return "خطا عدد انتخاب شده باید زیر 1000 باشد"  
+            end                                             
+            get_history(msg.to.peer_id, matches[2] + 1 , hi$
+        else                                                
+                         return "فقط مخصوص سوپرگروه می باشد"
+        end                                                 
+else                                                        
+return "فقط مخصوص مدیران گروه می باشد"                      
+end                                                         
+end                                                         
+return {                                                    
+    patterns = {                                            
+        '^[!/#](rmsg) (%d*)$'                                
+    },                                                      
+    run = run                                               
+}                                                           
+                                                            
+                                                            
+                                                            
+                                                            
+                                                            
+                                                            
+                                                            
+                                                            
+-- ^G Get Hel^O WriteOu^R Read Fi^Y Prev Pa^K Cut Tex^C Cur Pos
+-- ^X Exit   ^J Justify^W Where I^V Next Pa^U UnCut T^T To Spel
