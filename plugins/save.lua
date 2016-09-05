4f828a9c873ab6237845dfb431b1a10aa9b86b78
Abolfazl Dalton <abolfazl.k.Dalton@gmail.com>
2016-09-05 13:32:29 +0430

    Update save.lua

--- plugins/save.lua
+++ plugins/save.lua
@@ -9,6 +9,7 @@ if msg.text == "[/!#]help" then
   return "!res [username]/nReturns users name and id by username/n/n/n!log/nReturns group logs/n*Search for kick reasons using [#RTL|#spam|#lockmember]/n/n**You can use "#", "!", or "/" to begin all commands/n/n*Only owner can add members to SuperGroup/n(use invite link to invite)/n/n*Only moderators and owner can use block, ban, unban, newlink, link, setphoto, setname, lock, unlock, setrules, setabout and settings commands/n/n*Only owner can use res, setowner, promote, demote, and log commands"
 end
 end
+end
 
 return {
   description = "Chat With Robot Server", 
