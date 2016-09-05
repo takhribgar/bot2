2552f14048388eb994134c58eedb5c2bf16972c3
Abolfazl Dalton <abolfazl.k.Dalton@gmail.com>
2016-09-05 14:00:06 +0430

    Update save.lua

--- plugins/save.lua
+++ plugins/save.lua
@@ -6,7 +6,8 @@ if msg.text == "مریم" then
   return "با مامانیم چیکار داری؟"
 end
 if msg.text == "/help" then
-  return "Test\n\nTest1"
+  return "Help text:\n\n!gpinfo\nDisplays general info about the SuperGroup\n\n!admins
+Returns SuperGroup admins list\n\n!owner\nReturns group owner\n\n!modlist\nReturns Moderators list\n\n!bots\nLists bots in SuperGroup\n\n!who\nLists all users in SuperGroup\n\n!block Kicks a user from SuperGroup *Adds user to blocked list*\n\n!ban/nBans user from the SuperGroup\n\n!unban/nUnbans user from the SuperGroup\n\n!id/nReturn SuperGroup ID or user id *For userID's: !id @username or reply !id*\n!id from\nGet ID of user message is forwarded from\n\n!kickme\nKicks user from SuperGroup\n*Must be unblocked by owner or use join by pm to return*\n\n!setowner\nSets the SuperGroup owner\n\n!promote\n[username|id]\nPromote a SuperGroup moderator\n\n!demote\n[username|id]\nDemote a SuperGroup moderator\n\n!setname\nSets the chat name\n\n!setphoto\nSets the chat photo"
 end
 end
 
